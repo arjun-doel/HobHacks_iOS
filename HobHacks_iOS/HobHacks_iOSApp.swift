75833465d1b0c0e9b41f6e5aa20d4eace581d267
arjundoel <arjundoel@gmail.com>
2022-05-08 16:07:04 +0100

    Initial Commit

---
+++ HobHacks_iOS/HobHacks_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  HobHacks_iOSApp.swift
+//  HobHacks_iOS
+//
+//  Created by Arjun Doel on 08/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct HobHacks_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
