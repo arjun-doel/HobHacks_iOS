aa9c659b2377812ab1f211111ce40846e5f8880b
arjundoel <arjundoel@gmail.com>
2022-05-08 16:51:47 +0100

    Basic Card

--- HobHacks_iOS/ContentView.swift
+++ HobHacks_iOS/ContentView.swift
@@ -9,8 +9,38 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack(alignment: .top) {
+            ScrollView {
+                VStack(alignment: .leading, spacing: 5) {
+                    Image("demoRecipe")
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .frame(width: 280, height: 280)
+                        .mask(RoundedRectangle(cornerRadius: 30, style: . continuous))
+                    
+                    VStack(alignment: .leading, spacing: 2) {
+                        Text("Shakshouka")
+                            .font(.title2).bold()
+                            .foregroundColor(.black)
+                        Text("Maghreb")
+                            .font(.subheadline)
+                    }
+                    .padding()
+                    
+                }
+                .frame(width: 300, height: 400)
+                .background(.white)
+                .mask(RoundedRectangle(cornerRadius: 30, style: .continuous))
+                .shadow(color: .black.opacity(0.2), radius: 4, x: 0, y: 2)
+                .frame(width: 400)
+                .overlay(
+                    Image(systemName: "heart.fill")
+                        .foregroundColor(.purple)
+                        .font(.title)
+                        .offset(x: 110, y: 135)
+                )
+            }
+        }
     }
 }
 
